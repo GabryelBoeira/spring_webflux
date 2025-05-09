bbf6405c15037eb2ae7a9587fa2bd6f150c5b83d
gabryelBoeira <33075550+gabryelboeira@users.noreply.github.com>
2025-05-09 16:20:32 -0300

    Add logging for task creation and deletion actions
    Introduced SLF4J logging to the TaskController class. Added info-level logs to track task creation and deletion events, improving traceability and debugging capabilities.

--- task/src/main/java/com/gabryel/task/controller/TaskController.java
+++ task/src/main/java/com/gabryel/task/controller/TaskController.java
@@ -5,6 +5,8 @@ import com.gabryel.task.dto.TaskDetailDTO;
 import com.gabryel.task.dto.TaskSaveDTO;
 import com.gabryel.task.enums.TaskState;
 import com.gabryel.task.service.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -12,6 +14,8 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/task")
 public class TaskController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(TaskController.class);
 
     private final TaskService taskService;
 
@@ -34,7 +38,8 @@ public class TaskController {
 
     @PostMapping
     public ResponseEntity<Mono<TaskDetailDTO>> createTask(@RequestBody TaskSaveDTO task) {
-        var result = taskService.insertTask(task);
+        var result = taskService.insertTask(task)
+                .doOnNext(it -> LOGGER.info("Task created: {}", it));
         if (result == null) return ResponseEntity.badRequest().build();
 
         return ResponseEntity.ok(result);
@@ -48,6 +53,7 @@ public class TaskController {
         }
 
         return taskService.deleteById(id)
+                .doOnNext(it -> LOGGER.info("Task com id {} sendo deletada ", it))
                 .then(Mono.just(ResponseEntity.noContent().build()));
     }
 
