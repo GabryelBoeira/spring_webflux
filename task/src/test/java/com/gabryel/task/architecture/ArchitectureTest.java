28eaaa659ffe2eadf5cdd173c27eff32c760e58b
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-07-21 22:36:00 -0300

    ``` Adiciona testes de arquitetura com ArchUnit
    Foram criados testes para validar as boas práticas de arquitetura do projeto, como convenções de nomenclatura para controllers, services, repositórios e aspectos. Além disso, foram verificadas restrições de acesso entre pacotes, garantindo um design mais modular e aderente ao padrão definido.
    ```

---
+++ task/src/test/java/com/gabryel/task/architecture/ArchitectureTest.java
@@ -0,0 +1,106 @@
+package com.gabryel.task.architecture;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Test;
+
+public class ArchitectureTest {
+
+    private static final String CONTROLLER = "com.gabryel.task.controller..";
+    private static final String SERVICE = "com.gabryel.task.service..";
+    private static final String REPOSITORY = "com.gabryel.task.repository..";
+    private static final String ASPECT = "com.gabryel.task.aspect..";
+    private static final String DTO = "com.gabryel.task.dto..";
+    private static final String ENTITY = "com.gabryel.task.entity..";
+    private static final String CONVERTER = "com.gabryel.task.converter..";
+    private static final String EXCEPTION = "com.gabryel.task.exception..";
+
+    private final JavaClasses classes = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.gabryel");
+
+
+    @Test
+    void controllersShouldHaveCorrectNaming() {
+        ArchRuleDefinition.classes()
+                .that().resideInAPackage(CONTROLLER)
+                .should().haveSimpleNameEndingWith("Controller")
+                .check(classes);
+    }
+
+    @Test
+    void servicesShouldHaveCorrectNaming() {
+        ArchRuleDefinition.classes()
+                .that().resideInAPackage(SERVICE)
+                .should().haveSimpleNameEndingWith("Service")
+                .check(classes);
+    }
+
+    @Test
+    void repositoriesShouldHaveCorrectNaming() {
+        ArchRuleDefinition.classes()
+                .that().resideInAPackage(REPOSITORY)
+                .and().areInterfaces()
+                .and().areNotAnnotatedWith("org.springframework.stereotype.Repository")
+                .should().haveSimpleNameEndingWith("Repository")
+                .check(classes);
+    }
+
+    @Test
+    void aspectsShouldHaveCorrectNaming() {
+        ArchRuleDefinition.classes()
+                .that().resideInAPackage(ASPECT)
+                .should().haveSimpleNameEndingWith("Aspect")
+                .check(classes);
+    }
+
+    @Test
+    void onlyServiceCanAccessRepository() {
+        ArchRuleDefinition.noClasses()
+                .that().resideOutsideOfPackage(SERVICE)
+                .should().accessClassesThat().resideInAPackage(REPOSITORY)
+                .check(classes);
+    }
+
+    @Test
+    void onlyControllerAndServiceCanAccessDto() {
+        ArchRuleDefinition.noClasses()
+                //.that().resideOutsideOfPackages(EXCEPTION, CONTROLLER, SERVICE, CONVERTER)
+                .should().accessClassesThat().resideInAPackage(DTO)
+                .check(classes);
+    }
+
+    @Test
+    void onlyRepositoryCanAccessEntity() {
+        ArchRuleDefinition.noClasses()
+                .that().resideOutsideOfPackage(REPOSITORY)
+                .should().accessClassesThat().resideInAPackage(ENTITY)
+                .check(classes);
+    }
+
+    @Test
+    void controllersShouldOnlyBeAccessedByExternalPackages() {
+        ArchRuleDefinition.noClasses()
+                .that().resideOutsideOfPackage(CONTROLLER)
+                .should().accessClassesThat().resideInAPackage(CONTROLLER)
+                .check(classes);
+    }
+
+    @Test
+    void servicesShouldOnlyBeAccessedByControllerOrOtherServices() {
+        ArchRuleDefinition.noClasses()
+                .that().resideOutsideOfPackages(CONTROLLER, SERVICE)
+                .should().accessClassesThat().resideInAPackage(SERVICE)
+                .check(classes);
+    }
+
+    @Test
+    void aspectsShouldOnlyBeAccessedInternally() {
+        ArchRuleDefinition.noClasses()
+                .that().resideOutsideOfPackage(ASPECT)
+                .should().accessClassesThat().resideInAPackage(ASPECT)
+                .check(classes);
+    }
+}
