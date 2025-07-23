b4f3f27adbe969fde6e3600df3b36f95cb97609e
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2025-07-22 23:34:19 -0300

    Reativa aspectos comentados na classe TaskServiceAspect
    Os métodos e anotações de AOP (Aspect-Oriented Programming) comentados foram reativados, incluindo logs de início e fim de execução de métodos. Isso melhora a rastreabilidade e a observabilidade das execuções dentro do serviço.

--- task/src/main/java/com/gabryel/task/aspect/TaskServiceAspect.java
+++ task/src/main/java/com/gabryel/task/aspect/TaskServiceAspect.java
@@ -17,28 +17,28 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class TaskServiceAspect {
 
-//    private static final Logger LOGGER = LoggerFactory.getLogger(TaskServiceAspect.class);
-//
-//    private final TaskService taskService;
-//
-//    @Autowired
-//    public TaskServiceAspect(TaskService taskService) {
-//        this.taskService = taskService;
-//    }
-//
-//    // Pointcut (define o que sera observado da classe ao method)
-//    @Pointcut("execution(* com.gabryel.task.service.TaskService.*(..))")
-//    public void newTaskServiceExecution() {
-//    }
-//
-//    @Before(value = "newTaskServiceExecution()")
-//    public void beforeNewTask(final JoinPoint joinPoint) {
-//        LOGGER.info("Iniciou a execucao do metodo {}", joinPoint.getSignature().getName());
-//    }
-//
-//    @AfterReturning(value = "newTaskServiceExecution()")
-//    public void afterAllTask(final JoinPoint joinPoint) {
-//        LOGGER.info("Finalizou a execucao do metodo {}", joinPoint.getSignature().getName());
-//    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskServiceAspect.class);
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskServiceAspect(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    // Pointcut (define o que sera observado da classe ao method)
+    @Pointcut("execution(* com.gabryel.task.service.TaskService.*(..))")
+    public void newTaskServiceExecution() {
+    }
+
+    @Before(value = "newTaskServiceExecution()")
+    public void beforeNewTask(final JoinPoint joinPoint) {
+        LOGGER.info("Iniciou a execucao do metodo {}", joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning(value = "newTaskServiceExecution()")
+    public void afterAllTask(final JoinPoint joinPoint) {
+        LOGGER.info("Finalizou a execucao do metodo {}", joinPoint.getSignature().getName());
+    }
 
 }
